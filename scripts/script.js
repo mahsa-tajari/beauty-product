dca18a79c5a39ade061401bc70e5e6e345403a79
Mahsa <khodemahsatjr@gmail.com>
2023-05-23 10:28:25 +0330

    sections

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,15 @@
 const $ = document
 
 const hamburgerMenuIcon = $.querySelector('.hamburger-icon__wrapper')
+const body = $.querySelector('body')
+const darkLayerElem = $.querySelector('.dark-layer')
 
 function openHamburgerMenu(){
     const MenuElem = $.querySelector('.nav-list')
 
     MenuElem.classList.toggle('nav-list--open')
     hamburgerMenuIcon.classList.toggle('hamburger-icon__wrapper--open')
+    body.classList.toggle('overflow')
 }
 
 hamburgerMenuIcon.addEventListener('click',openHamburgerMenu)
